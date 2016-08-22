fd709a486b9207271a00c505f650382c511ee175
kenofnz <ken_kwan_master@hotmail.com>
2016-08-23 09:02:53 +1200

    Official Release Version 0.20 Update 1

--- get-jre-8.sh
+++ get-jre-8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-wget -O jre.tar.gz --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u101-b13/server-jre-8u101-linux-x64.tar.gz"
+JRE="http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jre-8u101-linux-x64.tar.gz"
+wget -O jre.tar.gz --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" $JRE
 tar zxf jre.tar.gz
 mv jre1.8.0_101 jre
 rm jre.tar.gz
